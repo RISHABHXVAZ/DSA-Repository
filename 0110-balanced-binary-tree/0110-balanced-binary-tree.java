9fb11a2f7bea0a88d51877677173c96cd822747a
Rishabh Srivastava <srivastavarishabh799@gmail.com>
2025-12-27 20:29:33 +0530

    Time: 0 ms (100%), Space: 45.8 MB (18.89%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -14,28 +14,20 @@
  * }
  */
 class Solution {
-    static List<TreeNode> preorder(TreeNode node, List<TreeNode> nodes){
-        if(node == null) return nodes;
-        nodes.add(node);
-        preorder(node.left, nodes);
-        preorder(node.right, nodes);
-        return nodes;
-    }
-    static int height(TreeNode node){
+    boolean isbalanced = true;
+     int height(TreeNode node){
         if(node == null) return 0;
         int left = height(node.left);
         int right = height(node.right);
-
-        if(Math.abs(right - left) <= 1) return 1 + Math.max(right,left);
-        else return -1;
+        
+        if(Math.abs(right - left) > 1){
+            isbalanced = false; 
+        }
+         return 1 + Math.max(right,left);
     }
     public boolean isBalanced(TreeNode root) {
-        List<TreeNode> nodes = new ArrayList<>();
-        nodes = preorder(root, nodes);
-        
-        for(TreeNode node: nodes){
-            if(height(node) == -1) return false; 
-        }
-        return true;
+        if(root == null) return true;
+        height(root);
+        return isbalanced;
     }
 }

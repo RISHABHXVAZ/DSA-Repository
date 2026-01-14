7825504341d21d1002f20b773f6195ebd4d1ea09
Rishabh Srivastava <srivastavarishabh799@gmail.com>
2026-01-14 19:15:57 +0530

    Time: 2 ms (83%), Space: 47.6 MB (86.23%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,14 +1,12 @@
 class Solution {
-    static void dfs(char[][] board, int r, int c, boolean[][] vis){
-        int rows = board.length;
-        int cols = board[0].length;
-        vis[r][c] = true;
-        int dx[] = {0,0,-1,1};
-        int dy[] = {-1,1,0,0};
+    static void dfs(char[][] board, int sr, int sc, boolean[][] vis){
+        vis[sr][sc] = true;
+        int[] dx = {1,-1,0,0};
+        int[] dy = {0,0,1,-1};
         for(int i = 0; i < 4; i++){
-            int x = r + dx[i];
-            int y = c + dy[i];
-            if(x >= 0 && x < rows && y >= 0 && y < cols){
+            int x = sr + dx[i];
+            int y = sc + dy[i];
+            if(x >= 0 && x < board.length && y >= 0 && y < board[0].length){
                 if(board[x][y] == 'O' && vis[x][y] == false){
                     dfs(board, x, y, vis);
                 }
@@ -16,43 +14,39 @@ class Solution {
         }
     }
     public void solve(char[][] board) {
-        int rows = board.length;
-        int cols = board[0].length;
-        boolean[][] vis = new boolean[rows][cols];
-
-    //checking 1st row
-        for(int j = 0; j < cols; j++){
-            if(board[0][j] == 'O' && vis[0][j] == false){
-                dfs(board,0,j,vis);
-            }
-        }
-    //checking 1st col
-        for(int i = 0; i < rows; i++){
-            if(board[i][0] == 'O' && vis[i][0] == false){
-                dfs(board,i,0,vis);
-            }
-        }
-    //checking last row
-        for(int j = 0; j < cols; j++){
-            if(board[rows-1][j] == 'O' && vis[rows-1][j] == false){
-                dfs(board,rows-1,j,vis);
-            }
-        }
-    //checking last column
-        for(int i = 0; i < rows; i++){
-            if(board[i][cols-1] == 'O' && vis[i][cols-1] == false){
-                dfs(board,i,cols-1,vis);
-            }
+        //i will traverse all the boundaries and if i get any zero then i will do dfs and mark all the connected zeros as visited
+        int m = board.length;
+        int n = board[0].length;
+        boolean[][] vis = new boolean[m][n];
+        
+        //upper boundary
+        for(int j = 0; j < n; j++){
+            if(board[0][j] == 'O' && !vis[0][j]) dfs(board, 0, j, vis);
         }
 
-        for(int i = 0; i < rows; i++){
-            for(int j = 0; j < cols; j++){
+        //left boundary
+        for(int i = 0; i < m; i++){
+            if(board[i][0] == 'O' && !vis[i][0]) dfs(board, i, 0, vis);
+        }
+
+        //right boundary
+        for(int i = 0; i < m; i++){
+            if(board[i][n-1] == 'O' && !vis[i][n-1]) dfs(board, i, n-1, vis);
+        }
+
+        //bottom boundary
+        for(int j = 0; j < n; j++){
+            if(board[m-1][j] == 'O' && !vis[m-1][j]) dfs(board, m-1, j, vis);
+        }
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
                 if(board[i][j] == 'O' && vis[i][j] == false){
                     board[i][j] = 'X';
                 }
             }
         }
 
-        return;
     }
+
 }

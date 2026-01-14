981b737bb6267f92ee99757bf31342ad8954ca07
Rishabh Srivastava <srivastavarishabh799@gmail.com>
2026-01-14 18:45:01 +0530

    Time: 18 ms (15.62%), Space: 49 MB (63.25%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,35 +1,34 @@
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
-        int rows = mat.length;
-        int cols = mat[0].length;
-        boolean vis[][] = new boolean[rows][cols];
-        int ans[][] = new int[rows][cols];
+        int m = mat.length;
+        int n = mat[0].length;
         Queue<int[]> q = new LinkedList<>();
-        for(int i = 0; i < rows; i++){
-            for(int j = 0; j < cols; j++){
+        boolean[][] vis = new boolean[m][n];
+        for(int i = 0; i < m ; i++){
+            for(int j = 0; j < n; j++){
                 if(mat[i][j] == 0){
-                    q.add(new int[]{i,j,0});
                     vis[i][j] = true;
+                    q.add(new int[]{i,j});
                 }
             }
         }
-        int dx[] = {-1,0,1,0};
-        int dy[] = {0,1,0,-1};
+
         while(!q.isEmpty()){
             int point[] = q.poll();
-            int r = point[0];
-            int c = point[1];
-            int dist = point[2];
-            ans[r][c] = dist;
+            int[] dx = {1,-1,0,0};
+            int[] dy = {0,0,1,-1};
             for(int i = 0; i < 4; i++){
-                int x = r + dx[i];
-                int y = c + dy[i];
-                if(x >= 0 && x < rows && y >= 0 && y < cols && vis[x][y] == false){
-                    vis[x][y] = true;
-                    q.add(new int[]{x,y,dist+1});
+                int x = point[0] + dx[i];
+                int y = point[1] + dy[i];
+                if(x >= 0 && x < m && y >= 0 && y < n){
+                    if(mat[x][y] == 1 && vis[x][y] == false){
+                        mat[x][y] = mat[point[0]][point[1]] + 1;
+                        vis[x][y] = true;
+                        q.add(new int[]{x,y});
+                    }
                 }
             }
         }
-        return ans;
+        return mat;
     }
 }

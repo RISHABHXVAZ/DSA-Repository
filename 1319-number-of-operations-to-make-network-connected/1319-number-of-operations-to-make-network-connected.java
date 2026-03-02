f5e4f8654cbe6bd838a27ac71babee6610194850
Rishabh Srivastava <srivastavarishabh799@gmail.com>
2026-03-02 10:50:44 +0530

    Time: 16 ms (21.77%), Space: 72.6 MB (18.56%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -1,40 +1,38 @@
 class Solution {
-    void bfs(int node, List<List<Integer>> adj, boolean[] vis){
+    void bfs(List<List<Integer>> adj, boolean[] vis, int nd){
         Queue<Integer> q = new LinkedList<>();
-        q.add(node);
-        vis[node] = true;
+        q.add(nd);
+        vis[nd] = true;
         while(!q.isEmpty()){
-            int n = q.poll();
-            for(int it: adj.get(n)){
-                if(!vis[it]){
-                    vis[it] = true;
-                    q.add(it);
-                }
+        int node = q.poll();
+        for(int it: adj.get(node)){
+            if(!vis[it]){
+                vis[it] = true; 
+                q.add(it);
             }
         }
-        return;
+    }
     }
     public int makeConnected(int n, int[][] connections) {
-        if(connections.length < n-1) return -1;
-        List<List<Integer>> adj = new ArrayList<>();
-        for(int i = 0; i < n; i++){
-            adj.add(new ArrayList<>());
+    if(connections.length < n-1) return -1;
+    List<List<Integer>> adj = new ArrayList<>();
+    for(int i = 0; i < n; i++){
+        adj.add(new ArrayList<>());
+    }
+    for(int i = 0; i < connections.length; i++){
+        int u = connections[i][0];
+        int v = connections[i][1];
+        adj.get(u).add(v);
+        adj.get(v).add(u);
+    }
+    int cnt = 0;
+    boolean[] vis = new boolean[n];
+    for(int i = 0; i < n; i++){
+        if(!vis[i]){
+            cnt++; 
+            bfs(adj, vis, i);
         }
-        for(int i = 0; i < connections.length; i++){
-            int u = connections[i][0];
-            int v = connections[i][1];
-            adj.get(u).add(v);
-            adj.get(v).add(u);
-        }
-        boolean[] vis = new boolean[n];
-        int components = 0;
-        for(int i = 0; i < n; i++){
-            if(!vis[i]){
-                bfs(i,adj,vis);
-                components++;
-            }
-        }
-        return components-1;
-
+    }
+    return cnt-1;
     }
 }

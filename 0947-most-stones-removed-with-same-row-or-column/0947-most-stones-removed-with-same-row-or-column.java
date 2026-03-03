dafd2a40ef9428f8886931073c36f9d544de4b88
Rishabh Srivastava <srivastavarishabh799@gmail.com>
2026-03-03 10:49:03 +0530

    Time: 20 ms (57.74%), Space: 47 MB (62.41%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
@@ -1,53 +1,56 @@
 class Solution {
     List<Integer> parent = new ArrayList<>();
-    List<Integer> size = new ArrayList<>(); 
+    List<Integer> size = new ArrayList<>();
     void initialize(int n){
         for(int i = 0; i < n; i++){
             parent.add(i);
             size.add(1);
         }
     }
-    int findParent(int u){
+
+    int findUPar(int u){
         if(u == parent.get(u)) return u;
-        int ulp_u = findParent(parent.get(u));
-        parent.set(u, ulp_u);
-        return ulp_u;
+        int par = findUPar(parent.get(u));
+        parent.set(u, par);
+        return par;
     }
+
     void UnionBySize(int u, int v){
-        int ulp_u = findParent(u);
-        int ulp_v = findParent(v);
-        if(ulp_u == ulp_v) return;
-        if(size.get(ulp_u) < size.get(ulp_v)){
-            parent.set(ulp_u, ulp_v);
-            size.set(ulp_v , size.get(ulp_u) + size.get(ulp_v));
+        int upar = findUPar(u);
+        int vpar = findUPar(v);
+        if(upar == vpar) return;
+        else if(size.get(u) < size.get(v)){
+            parent.set(upar, vpar);
+            size.set(vpar, size.get(upar) + size.get(vpar));
         }else{
-            parent.set(ulp_v, ulp_u);
-            size.set(ulp_u, size.get(ulp_v) + size.get(ulp_u));
+            parent.set(upar, vpar);
+            size.set(upar, size.get(upar) + size.get(vpar));
         }
-        return;
-    }
+    } 
     public int removeStones(int[][] stones) {
-        int maxRow = 0;
-        int maxCol = 0;
+        int maxrow = Integer.MIN_VALUE;
+        int maxcol = Integer.MIN_VALUE;
         for(int i = 0; i < stones.length; i++){
-            maxRow = Math.max(maxRow, stones[i][0]);
-            maxCol = Math.max(maxCol, stones[i][1]);
+            maxrow = Math.max(maxrow, stones[i][0]);
+            maxcol = Math.max(maxcol, stones[i][1]);
         }
-        Map<Integer, Integer> mpp = new HashMap<>();
-        int n = maxRow + maxCol + 2;
+        
+        int n = maxrow + maxcol + 2;
         initialize(n);
+        
+        Map<Integer, Integer> mpp = new HashMap<>();
         for(int i = 0; i < stones.length; i++){
             int u = stones[i][0];
             int v = stones[i][1];
-            UnionBySize(u, v+maxRow+1);
-            mpp.put(u, 1);
-            mpp.put(v+maxRow+1,1);
+            UnionBySize(u,v+maxrow+1);
+            mpp.put(u,1);
+            mpp.put(v + maxrow + 1, 1);
         }
         int comp = 0;
         for(int it: mpp.keySet()){
-            if(findParent(it) == it) comp++;
+            if(parent.get(it) == it) comp++;
         }
-        return stones.length-comp;
+        return stones.length - comp;
         
     }
 }

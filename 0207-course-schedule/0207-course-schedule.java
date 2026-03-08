378c0486bf6d65fcb710bbbef010e1b05fb3d213
Rishabh Srivastava <srivastavarishabh799@gmail.com>
2026-03-08 23:30:55 +0530

    Time: 6 ms (74.48%), Space: 47 MB (87.35%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,33 +1,34 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-       List<List<Integer>> adj = new ArrayList<>();
-       for(int i = 0; i < numCourses; i++){
-        adj.add(new ArrayList<>());
-       } 
-       for(int i = 0; i < prerequisites.length; i++){
-        adj.get(prerequisites[i][0]).add(prerequisites[i][1]);
-       }
-
-       int[] indegree = new int[numCourses];
-       for(int i = 0; i < numCourses; i++){
-        for(int j : adj.get(i)){
-            indegree[j]++;
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i = 0; i < numCourses; i++){
+            adj.add(new ArrayList<>());
         }
-       }
+        for(int i = 0; i < prerequisites.length; i++){
+            int u = prerequisites[i][0];
+            int v = prerequisites[i][1];
+            adj.get(u).add(v);
+        }
+        int[] indegree = new int[numCourses];
+        for(int i = 0; i < numCourses; i++){
+            for(int it: adj.get(i)){
+                indegree[it]++;
+            }
+        }
         Queue<Integer> q = new LinkedList<>();
-       for(int i = 0; i < numCourses; i++){
-        if(indegree[i] == 0) q.add(i);
-       }
-       int cnt = 0;
-       while(!q.isEmpty()){
-        int node = q.poll();
-        cnt++;
-        for(int it: adj.get(node)){
-            indegree[it]--;
-            if(indegree[it] == 0) q.add(it);
+        for(int i = 0; i < numCourses; i++){
+            if(indegree[i] == 0) q.add(i);
         }
-       }
-       if(cnt != numCourses) return false;
-       return true;
+        int cnt = 0;
+        while(!q.isEmpty()){
+            int node = q.poll();
+            cnt++;
+            for(int it: adj.get(node)){
+                indegree[it]--;
+                if(indegree[it] == 0) q.add(it);
+            }
+        }
+        if(cnt != numCourses) return false;
+        return true;
     }
 }

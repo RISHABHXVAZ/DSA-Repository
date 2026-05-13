7b8cf6943e572eaa42e44278de7e9731115dfb5a
Rishabh Srivastava <srivastavarishabh799@gmail.com>
2026-05-13 09:47:01 +0530

    Time: 3 ms (85.75%), Space: 45.3 MB (72.86%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -3,21 +3,17 @@ class Solution {
         int m = triangle.size();
         int n = triangle.get(triangle.size()-1).size();
 
-        int[][] dp = new int[m][n];
+        int[] front = new int[n];
+        for(int i = 0; i < n; i++) front[i] = triangle.get(m-1).get(i);
 
-        dp[0][0] = triangle.get(0).get(0);
+        for(int i = m-2; i >= 0; i--){
+            int[] curr = new int[triangle.get(i).size()];
+            for(int j = 0; j < curr.length; j++){
+                curr[j] = triangle.get(i).get(j) + Math.min(front[j], front[j+1]);
+            }
+            front = curr;
+        }
 
-        for(int i = 1; i < m; i++){
-            for(int j = 0; j < triangle.get(i).size(); j++){
-                if(j == 0) dp[i][0] = triangle.get(i).get(0) + dp[i-1][0];
-                else if(j == triangle.get(i).size()-1) dp[i][j] = triangle.get(i).get(j)+dp[i-1][j-1];
-                else dp[i][j] = triangle.get(i).get(j) + Math.min(dp[i-1][j], dp[i-1][j-1]);       
-             }
-        }
-        int ans = Integer.MAX_VALUE;
-        for(int j = 0; j < n; j++){
-            ans = Math.min(ans, dp[m-1][j]);
-        }
-        return ans;
+        return front[0];
     }
 }

86640f5f11b44b9f0989cd6c729848914ec4210a
Rishabh Srivastava <srivastavarishabh799@gmail.com>
2026-05-16 10:23:11 +0530

    Time: 129 ms (5.47%), Space: 57.8 MB (39.01%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,26 +1,24 @@
 class Solution {
-    static boolean func(int idx, int[] nums, int[][] dp, int sum, int target){
-        if(sum == target) return true;
-        if(idx == 0) return false;
-        if(dp[idx][sum] != -1) return dp[idx][sum] == 1;
-        boolean nottake = func(idx-1, nums, dp,sum, target);
-        boolean take = false;
-        if(sum >= nums[idx]) take = func(idx-1, nums, dp,sum-nums[idx], target);
-
-        dp[idx][sum] = take||nottake ? 1: 0;
-
-        return take || nottake;
-    }
     public boolean canPartition(int[] nums) {
         int n = nums.length;
         
         int sum = 0;
-        for(int num : nums) sum += num;
-        if(sum % 2 != 0) return false;
+        for(int num: nums) sum += num;
+        if((sum & 1) == 1) return false;
+        boolean[][] dp = new boolean[n][sum + 1];
 
-        int[][] dp = new int[n][sum+1];
+        for(int i = 0; i < n; i++) Arrays.fill(dp[i], false);
+        dp[0][sum/2] = true;
 
-        for(int i = 0; i < n; i++) Arrays.fill(dp[i], -1);
-        return func(n-1, nums, dp, sum, sum/2);
+        for(int idx = 1; idx < n; idx++){
+            for(int target = 0; target <= sum; target++){
+                boolean nottaken = dp[idx-1][target];
+                boolean taken = false;
+                if(nums[idx] <= target) taken = dp[idx-1][target-nums[idx]];
+                dp[idx][target] = taken||nottaken;
+            }
+        }
+
+        return dp[n-1][sum];
     }
 }

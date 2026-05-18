2b151acc9f644fb4ece16876a024fa9f28276bca
Rishabh Srivastava <srivastavarishabh799@gmail.com>
2026-05-18 21:57:52 +0530

    Time: 11 ms (59.32%), Space: 57 MB (32.35%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -14,7 +14,22 @@ class Solution {
     public int change(int amount, int[] coins) {
         int n = coins.length;
         int[][] dp = new int[n][amount+1];
-        for(int i = 0; i < n; i++) Arrays.fill(dp[i], -1);
-        return func(n-1, amount, coins, dp);
+        
+        if(coins[0] == 0) dp[0][0] = 2;
+        else dp[0][0] = 1;
+
+        for(int t = 1; t <= amount; t++){
+            if(t % coins[0] == 0) dp[0][t] = 1;
+        }
+        
+        for(int i = 1; i < n; i++){
+            for(int t = 0; t <= amount; t++){
+                int nottake = dp[i-1][t];
+                int take = 0;
+                if(coins[i] <= t) take = dp[i][t - coins[i]];
+                dp[i][t] = take + nottake;
+            }
+        }
+        return dp[n-1][amount];
     }
 }

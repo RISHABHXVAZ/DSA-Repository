549af53b23a9299bf55d9dd145f4298e86e5905f
Rishabh Srivastava <srivastavarishabh799@gmail.com>
2026-05-18 11:35:22 +0530

    Time: 5 ms (78.24%), Space: 44.9 MB (45.05%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,18 +1,28 @@
 class Solution {
-    int func(int idx, int[] nums, int target){
+    //memorisation method
+    int func(int idx, int[] nums, int tgt, int[][] dp){
         if(idx == 0){
-            if(target == 0 && nums[0] == 0) return 2;
-            if(nums[idx] == target || nums[idx] == -target) return 1;
+            if(tgt == 0 && nums[0] == 0) return 2;
+            if(tgt == 0 || nums[idx] == tgt) return 1;
             else return 0;
         }
-        
-        int addplus = func(idx-1, nums, target - nums[idx]);
-        int addminus = func(idx-1, nums, target + nums[idx]);
-        return addplus + addminus;
+        if(dp[idx][tgt] != -1) return dp[idx][tgt];
+        int nottake = func(idx-1, nums, tgt, dp);
+        int take = 0;
+        if(nums[idx] <= tgt) take = func(idx-1, nums, tgt - nums[idx], dp);
+        dp[idx][tgt] = nottake + take;
+        return dp[idx][tgt];
     }
     public int findTargetSumWays(int[] nums, int target) {
         int n = nums.length;
-        
-        return func(n-1, nums,target);
+        int tsum = 0;
+        for(int num: nums) tsum += num;
+        if(target > tsum || (tsum-target) % 2 != 0) return 0;
+        int tgt = (tsum - target)/2;
+        int[][] dp = new int[n][tgt+1];
+
+        for(int i = 0; i < n; i++) Arrays.fill(dp[i], -1);
+
+        return func(n-1, nums, tgt, dp);
     }
 }

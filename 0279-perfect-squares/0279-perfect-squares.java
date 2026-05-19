7ac296d39fa81c2ac65a463f53ececed8d5db0bb
Rishabh Srivastava <srivastavarishabh799@gmail.com>
2026-05-19 10:13:34 +0530

    Time: 158 ms (13.88%), Space: 62.7 MB (6.21%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,25 +1,24 @@
 class Solution {
-    int func(int i , int[] nums, int target, int[][] dp){
-        if(i == 0){
-            if(target % (int)Math.pow(nums[0], 2) == 0) return (int)(target/Math.pow(nums[0], 2));
-            else return 0;
-        }
-        if(dp[i][target] != -1) return dp[i][target];
-
-        int nottake = func(i-1, nums, target, dp);
-        int take = (int)1e9;
-        if((int)Math.pow(nums[i], 2) <= target) take = 1 + func(i, nums, target - (int)Math.pow(nums[i],2), dp);
-        
-        dp[i][target] = (int)Math.min(nottake, take);
-        return dp[i][target];
-    }
+    
     public int numSquares(int n) {
       int h = (int)Math.floor(Math.sqrt(n));  
       int[] nums = new int[h];
       for(int i = 0; i < h; i++) nums[i] = i+1;
     
       int[][] dp = new int[h][n+1];
-      for(int i = 0; i < h; i++) Arrays.fill(dp[i], -1);
-      return func(h-1, nums, n, dp);
+      int val = (int)Math.pow(nums[0], 2);
+      for(int t = 0; t <= n; t++){
+        if(t % val == 0) dp[0][t] = t/val;
+      }
+
+      for(int i = 1; i < h; i++){
+        for(int t = 0; t <= n; t++){
+            int nottake = dp[i-1][t];
+            int take = (int)1e9;
+            if((int)Math.pow(nums[i], 2) <= t) take = 1 + dp[i][t-(int)Math.pow(nums[i], 2)];
+            dp[i][t] = Math.min(nottake, take);
+        }
+      }
+      return dp[h-1][n];
     }
 }

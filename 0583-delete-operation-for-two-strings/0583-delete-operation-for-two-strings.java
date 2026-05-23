a09ffa6a4ee3f92bb2ee12b8fa60f506968ad2f6
Rishabh Srivastava <srivastavarishabh799@gmail.com>
2026-05-23 10:28:52 +0530

    Time: 6 ms (93.68%), Space: 46.2 MB (89.34%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
@@ -16,8 +16,18 @@ class Solution {
         int l1 = word1.length();
         int l2 = word2.length();
 
-        int[][] dp = new int[l1][l2];
-        for(int i = 0; i < l1; i++) Arrays.fill(dp[i], -1);
-        return func(0,0,word1,word2, dp);
+        int[][] dp = new int[l1+1][l2+1];
+
+        for(int j = 0; j <= l2; j++) dp[l1][j] = l2 - j;
+        for(int i = 0; i <= l1; i++) dp[i][l2] = l1 - i;
+
+        for(int i = l1-1; i >= 0; i--){
+            for(int j = l2-1; j >= 0; j--){
+                if(word1.charAt(i) == word2.charAt(j)) dp[i][j] = dp[i+1][j+1];
+                else dp[i][j] = 1 + Math.min(dp[i][j+1], dp[i+1][j]);
+            }
+        }
+        
+        return dp[0][0];
     }
 }

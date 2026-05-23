329f2c90a0996e0d9cc03aec3cec2d5fddcc5f60
Rishabh Srivastava <srivastavarishabh799@gmail.com>
2026-05-23 10:41:12 +0530

    Time: 6 ms (93.68%), Space: 46 MB (92.29%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
@@ -16,18 +16,21 @@ class Solution {
         int l1 = word1.length();
         int l2 = word2.length();
 
-        int[][] dp = new int[l1+1][l2+1];
+       int[] curr = new int[l2+1];
+        int[] next = new int[l2+1];
 
-        for(int j = 0; j <= l2; j++) dp[l1][j] = l2 - j;
-        for(int i = 0; i <= l1; i++) dp[i][l2] = l1 - i;
-
-        for(int i = l1-1; i >= 0; i--){
-            for(int j = l2-1; j >= 0; j--){
-                if(word1.charAt(i) == word2.charAt(j)) dp[i][j] = dp[i+1][j+1];
-                else dp[i][j] = 1 + Math.min(dp[i][j+1], dp[i+1][j]);
-            }
-        }
+        for(int j = 0; j <= l2; j++) next[j] = l2 - j;
         
-        return dp[0][0];
+        for(int i = l1-1; i >= 0; i--){
+            curr[l2] = l1 - i;
+            for(int j = l2-1; j >= 0; j--){
+                if(word1.charAt(i) == word2.charAt(j)) curr[j] = next[j+1];
+                else curr[j] = 1 + Math.min(curr[j+1], next[j]);
+            }
+            next = curr;
+            curr = new int[l2+1];
+        }
+
+        return next[0];
     }
 }

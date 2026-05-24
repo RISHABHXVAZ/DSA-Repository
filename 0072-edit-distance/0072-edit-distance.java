623f7e4a43a5925a65d176f755882bf56c686449
Rishabh Srivastava <srivastavarishabh799@gmail.com>
2026-05-24 21:48:32 +0530

    Time: 5 ms (69.71%), Space: 47.4 MB (5.88%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -22,33 +22,33 @@ class Solution {
         int n = word1.length();
         int m = word2.length();
 
-        int[][] dp = new int[n][m];
-        for(int i = 0; i < n; i++) Arrays.fill(dp[i],-1);
+        // int[][] dp = new int[n][m];
+        // for(int i = 0; i < n; i++) Arrays.fill(dp[i],-1);
 
-        return func(n-1, m-1, word1, word2, dp);
-        // int[][] dp = new int[n+1][m+1];
-        // dp[0][0] = 0;
-        // for(int j = 1; j <= m; j++){
-        //     dp[0][j] = j+1;
-        // }
-        // for(int i = 1; i <= n; i++){
-        //     dp[i][0] = i+1;
-        // }
+        // return func(n-1, m-1, word1, word2, dp);
+        int[][] dp = new int[n+1][m+1];
+        dp[0][0] = 0;
+        for(int j = 1; j <= m; j++){
+            dp[0][j] = j;
+        }
+        for(int i = 1; i <= n; i++){
+            dp[i][0] = i;
+        }
 
-        // for(int i = 1; i <= n; i++){
-        //     for(int j = 1; j <= m; j++){
-        //         if(word1.charAt(i-1) != word2.charAt(j-1)){
-        //             int op1 = 1 + dp[i-1][j];
-        //             int op2 = 1 + dp[i][j-1];
-        //             int op3 = 1 + dp[i-1][j-1];
-        //             dp[i][j] = Math.min(op1, Math.min(op2, op3));
-        //         }else{
-        //             dp[i][j] = dp[i-1][j-1];
-        //         }
-        //     }
-        // }
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                if(word1.charAt(i-1) != word2.charAt(j-1)){
+                    int op1 = 1 + dp[i-1][j];
+                    int op2 = 1 + dp[i][j-1];
+                    int op3 = 1 + dp[i-1][j-1];
+                    dp[i][j] = Math.min(op1, Math.min(op2, op3));
+                }else{
+                    dp[i][j] = dp[i-1][j-1];
+                }
+            }
+        }
 
-        // return dp[n][m];
+        return dp[n][m];
         
     }
 }

c6bc50d7e163071c4dcee2784712ffe8690f1464
Rishabh Srivastava <srivastavarishabh799@gmail.com>
2026-05-25 10:12:48 +0530

    Time: 117 ms (5.27%), Space: 62.9 MB (20.2%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -0,0 +1,36 @@
+class Solution {
+    int func(int i, int j, String s, String p, int[][] dp){
+        if(i < 0 && j < 0) return 1;
+        if(i < 0){
+            while(j >= 0){
+                if(p.charAt(j) != '*') return 0;
+                j--;
+            }
+            return 1;
+        }
+        if(j < 0) return 0;
+
+        if(dp[i][j] != -1) return dp[i][j];
+        if(s.charAt(i) == p.charAt(j) || p.charAt(j) == '?'){
+            dp[i][j] = func(i-1,j-1,s,p,dp);
+        }else if(p.charAt(j) == '*'){
+            int ans = 0;
+            for(int k = 0; k <= i+1; k++){
+                ans = (ans | func(i-k,j-1, s,p,dp));
+            }
+            dp[i][j] = ans;
+        }else dp[i][j] = 0;
+
+        return dp[i][j];
+    }
+    public boolean isMatch(String s, String p) {
+       int n = s.length();
+       int m = p.length();
+    
+        int[][] dp = new int[n][m];
+        for(int i = 0; i < n; i++){
+            Arrays.fill(dp[i], -1);
+        }
+        return func(n-1,m-1,s,p,dp) == 1 ? true: false;
+    }
+}

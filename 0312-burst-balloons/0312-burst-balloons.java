b7ef2a9d941c345714cc12f494a47bba00c5a40a
Rishabh Srivastava <srivastavarishabh799@gmail.com>
2026-05-31 16:46:42 +0530

    Time: 45 ms (68.95%), Space: 44.6 MB (31.83%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -1,14 +1,14 @@
 class Solution {
-    int func(int i, int j, int[] nums){
+    int func(int i, int j, int[] nums, int[][] dp){
         if(i > j) return 0;
-        int ans = Integer.MIN_VALUE;
+        if(dp[i][j] != Integer.MIN_VALUE) return dp[i][j];
         int steps = 0;
         for(int k = i; k <= j; k++){
 
-            steps = nums[i-1]*nums[k]*nums[j+1] + func(i, k-1, nums) + func(k+1, j, nums);
-            ans = Math.max(ans, steps);
+            steps = nums[i-1]*nums[k]*nums[j+1] + func(i, k-1, nums,dp) + func(k+1, j, nums,dp);
+            dp[i][j] = Math.max(dp[i][j], steps);
         }
-        return ans;
+        return dp[i][j];
     }
     public int maxCoins(int[] nums) {
         int n = nums.length;
@@ -19,6 +19,21 @@ class Solution {
             newarr[i] = nums[i-1];
         }
 
-        return func(1, n, newarr);
+        int[][] dp = new int[n+2][n+2];
+      
+        for(int i = n; i >= 1; i--){
+            for(int j = 1; j <= n; j++){
+                if(i > j) dp[i][j] = 0;
+                else{
+                    int steps = 0;
+                    dp[i][j] = Integer.MIN_VALUE;
+                    for(int k = i; k <= j; k++){
+                        steps = newarr[i-1]*newarr[k]*newarr[j+1] + dp[i][k-1] + dp[k+1][j];
+                        dp[i][j] = Math.max(dp[i][j], steps);
+                    }
+                }
+            }
+        }
+        return dp[1][n];
     }
 }

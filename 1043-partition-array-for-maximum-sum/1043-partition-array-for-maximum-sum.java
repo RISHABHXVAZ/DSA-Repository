891629b315ffd78b12886442f71b5f6807d19e44
Rishabh Srivastava <srivastavarishabh799@gmail.com>
2026-06-01 19:26:08 +0530

    Time: 8 ms (21.65%), Space: 43.9 MB (64.44%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
@@ -1,13 +1,13 @@
 class Solution {
-    int func(int i, int j, int x, int[] arr, int[] dp){
-        if(i > j) return 0;
+    int func(int i , int x, int[] arr, int[] dp){
+        if(i > arr.length-1) return 0;
         if(dp[i] != -1) return dp[i];
         int maxval = arr[i];
         int steps = 0;
         dp[i] = Integer.MIN_VALUE;
-        for(int k = i; k < i + x && k <= j; k++){
+        for(int k = i; k < i + x && k <= arr.length-1; k++){
             maxval = Math.max(maxval, arr[k]);
-            steps = (k-i+1)*maxval + func(k+1, j, x, arr, dp);
+            steps = (k-i+1)*maxval + func(k+1, x, arr, dp);
             dp[i] = Math.max(dp[i], steps);
         }
         return dp[i];
@@ -16,6 +16,6 @@ class Solution {
         int n = arr.length;
         int[] dp = new int[n];
         for(int i = 0; i < n; i++) Arrays.fill(dp, -1);
-        return func(0, n-1, k, arr, dp);
+        return func(0, k, arr, dp);
     }
 }

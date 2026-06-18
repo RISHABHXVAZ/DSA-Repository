8394391cb5bff3309b0325238873b4aca11bb3d6
Rishabh Srivastava <srivastavarishabh799@gmail.com>
2026-06-18 10:43:21 +0530

    Time: 0 ms (100%), Space: 46.8 MB (96.61%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -1,28 +1,28 @@
 class Solution {
-    static boolean dfs(int node, int col, int[][] graph, int[] color){
+    boolean dfs(int node, int col, int[][] graph, int[] color){
         color[node] = col;
+
         for(int i = 0; i < graph[node].length; i++){
-            int neigh = graph[node][i];
-            if(color[neigh] == -1){
-                if(!dfs(neigh, 1-col, graph, color)){
-                    return false;
-                }
-            }
-            else if(color[neigh] == col) return false;
+            int ngh = graph[node][i];
+            if(color[ngh] == col) return false;
+
+            if(color[ngh] == -1 && !dfs(ngh, 1 - col, graph, color)) return false;
         }
+
         return true;
     }
     public boolean isBipartite(int[][] graph) {
-        int[] color = new int[graph.length];
-        for(int i = 0; i < graph.length; i++) color[i] = -1;
-        
-        for(int i = 0; i < graph.length; i++){
+        int n = graph.length;
+
+        int[] color = new int[n];
+        Arrays.fill(color, -1);
+
+        for(int i = 0; i < n; i++){
             if(color[i] == -1){
-                if(!dfs(i,0,graph,color)){
-                    return false;
-                }
+                if(!dfs(i, 0, graph, color)) return false;
             }
         }
+
         return true;
     }
 }

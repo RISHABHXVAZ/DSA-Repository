3bb6aaa7fbae2c5d678f6822a62f1a83a2c4e708
Rishabh Srivastava <srivastavarishabh799@gmail.com>
2026-06-18 11:37:41 +0530

    Time: 6 ms (45.23%), Space: 48.1 MB (14.45%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,41 +1,47 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
+        int n = numCourses;
+
         List<List<Integer>> adj = new ArrayList<>();
-        for(int i = 0; i < numCourses; i++){
-            adj.add(new ArrayList<>());
+
+        for(int i = 0; i < n; i++) adj.add(new ArrayList<>());
+
+        for(int i = 0; i < prerequisites.length;i++){
+            int u = prerequisites[i][0];
+            int v = prerequisites[i][1];
+
+            adj.get(v).add(u);
         }
-        for(int i = 0; i < prerequisites.length; i++){
-            adj.get(prerequisites[i][0]).add(prerequisites[i][1]);
-        }
-        int[] indegree = new int[numCourses];
-        for(int i = 0; i < numCourses; i++){
+
+        List<Integer> temp = new ArrayList<>();
+        int[] indegree = new int[n];
+
+        for(int i = 0; i < n; i++){
             for(int it: adj.get(i)){
                 indegree[it]++;
             }
         }
 
         Queue<Integer> q = new LinkedList<>();
-        for(int i = 0; i < numCourses; i++){
+        for(int i = 0; i < n; i++){
             if(indegree[i] == 0) q.add(i);
         }
-        List<Integer> topo = new ArrayList<>();
+
         while(!q.isEmpty()){
             int node = q.poll();
-            topo.add(node);
+            temp.add(node);
             for(int it: adj.get(node)){
                 indegree[it]--;
                 if(indegree[it] == 0) q.add(it);
             }
         }
-        if(topo.size() == numCourses){
-            Collections.reverse(topo);
-            int ans[] = new int[topo.size()];
-            int k = 0;
-            for(int it: topo){
-                ans[k++] = it;
-            }
-            return ans;
+
+        if(temp.size() != n) return new int[0];
+
+        int[] ans = new int[n];
+        for(int i = 0; i < n; i++){
+            ans[i] = temp.get(i);
         }
-        else return new int[0];
+        return ans;
     }
 }

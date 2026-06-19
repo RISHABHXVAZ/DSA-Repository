ee369c7c124b9511b23ea8e3a3e8828a9d2f8c0b
Rishabh Srivastava <srivastavarishabh799@gmail.com>
2026-06-19 15:21:31 +0530

    Time: 31 ms (12.67%), Space: 47.8 MB (33.75%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,34 +1,32 @@
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
         int n = grid.length;
-        if(grid[0][0] == 1 || grid[n-1][n-1] == 1) return -1;
-        int dist[][] = new int[n][n];
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                dist[i][j] = Integer.MAX_VALUE;
-            }
-        }
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[2]-b[2]);
-        pq.add(new int[]{0,0,1});
+        if(grid[0][0] == 1) return -1;
+
+        int[][] dist = new int[n][n];
+        for(int i = 0; i < n; i++) Arrays.fill(dist[i], Integer.MAX_VALUE);
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[0]-b[0]);
+        pq.add(new int[]{1, 0, 0});
         dist[0][0] = 1;
+
+        int[] dx = {1, 1, 1, -1, -1 , -1, 0, 0};
+        int[] dy = {0, 1, -1, 0, 1, -1, 1, -1};
         while(!pq.isEmpty()){
-            int row = pq.peek()[0];
-            int col = pq.peek()[1];
-            int distance = pq.peek()[2];
-            pq.remove();
-            int dx[] = {-1,-1,-1,1,1,1,0,0};
-            int dy[] = {-1,0,1,-1,0,1,-1,1};
+            int[] p = pq.poll();
+            int d = p[0], r = p[1], c = p[2];
+
             for(int i = 0; i < 8; i++){
-                int nr = row + dx[i];
-                int nc = col + dy[i];
-                if(nr >= 0 && nr < n && nc >= 0 && nc < n){
-                    if(grid[nr][nc] == 0 && (distance+1 < dist[nr][nc])){
-                    dist[nr][nc] = distance+1;
-                    pq.add(new int[]{nr,nc,dist[nr][nc]});
-                    }
+                int nx = r + dx[i];
+                int ny = c + dy[i];
+
+                if(nx >= 0 && nx < n && ny >= 0 && ny < n && grid[nx][ny] == 0 && dist[nx][ny] > d + 1){
+                    dist[nx][ny] = d + 1;
+                    pq.add(new int[]{dist[nx][ny], nx, ny});
                 }
             }
         }
-        return dist[n-1][n-1] != Integer.MAX_VALUE ? dist[n-1][n-1] : -1;
+
+        return dist[n-1][n-1] == Integer.MAX_VALUE ? -1 : dist[n-1][n-1];
     }
 }

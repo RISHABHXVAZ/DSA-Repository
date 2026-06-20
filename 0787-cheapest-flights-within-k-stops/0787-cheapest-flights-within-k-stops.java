e840d506bb7155e08e53600c15b056dbb4122e07
Rishabh Srivastava <srivastavarishabh799@gmail.com>
2026-06-20 14:43:44 +0530

    Time: 7 ms (41.07%), Space: 46.4 MB (69.58%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,33 +1,31 @@
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
         List<List<int[]>> adj = new ArrayList<>();
-        for(int i = 0; i < n; i++){
-            adj.add(new ArrayList<>());
-        }
+        for(int i = 0; i < n; i++) adj.add(new ArrayList<>());
+
         for(int i = 0; i < flights.length; i++){
-            int u = flights[i][0];
-            int v = flights[i][1];
+            int from = flights[i][0];
+            int to = flights[i][1];
             int price = flights[i][2];
-            adj.get(u).add(new int[]{v,price});
+            adj.get(from).add(new int[]{to, price});
         }
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{0,src,0});
         int[] dist = new int[n];
-        Arrays.fill(dist,Integer.MAX_VALUE);
-        dist[src] = 0;
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{0, src, -1});
+
         while(!q.isEmpty()){
-            int stops = q.peek()[0];
-            int node = q.peek()[1];
-            int distance = q.peek()[2];
-            q.remove();
-            if(stops > k) break;
+            int[] p = q.poll();
+            int d = p[0], node = p[1], stops = p[2]; 
             for(int[] it: adj.get(node)){
-                if(distance + it[1] < dist[it[0]]){
-                    dist[it[0]] = distance + it[1];
-                    q.add(new int[]{stops+1,it[0],dist[it[0]]});
+                int ngh = it[0], w = it[1];
+                if(dist[ngh] > w + d && stops+1 <= k){
+                    dist[ngh] = w+d;
+                    q.add(new int[]{dist[ngh], ngh, stops+1});
                 }
             }
         }
-        return dist[dst] == Integer.MAX_VALUE?-1:dist[dst]; 
+
+        return dist[dst] == Integer.MAX_VALUE ? -1 : dist[dst];
     }
 }

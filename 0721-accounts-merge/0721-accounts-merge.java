287a9fcf58057d12e870b9a2ea9f05af84a9371e
Rishabh Srivastava <srivastavarishabh799@gmail.com>
2026-06-30 12:15:15 +0530

    Time: 29 ms (79.6%), Space: 49.3 MB (92.61%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.java
+++ 0721-accounts-merge/0721-accounts-merge.java
@@ -1,75 +1,65 @@
 class Solution {
-    List<Integer> parent = new ArrayList<>();
-    List<Integer> size = new ArrayList<>();
+    int[] size;
+    int[] par;
 
-    void initialize(int n){
-        for(int i = 0; i < n; i++){
-            parent.add(i);
-            size.add(1);
-        }
+    void initialise(int n){
+        size = new int[n];
+        par = new int[n];
+        Arrays.fill(size, 1);
+        for(int i = 0; i < n; i++) par[i] = i;
     }
 
-    int findUPar(int u){
-        if(u == parent.get(u)) return u;
-        int par = findUPar(parent.get(u));
-        parent.set(u, par);
-        return par;
-    }
-
-    void UnionBySize(int u, int v){
-        int upar = findUPar(u);
-        int vpar = findUPar(v);
-
+    void Union(int u, int v){
+        int upar = find(u);
+        int vpar = find(v);
         if(upar == vpar) return;
 
-        if(size.get(upar) < size.get(vpar)){
-            parent.set(upar, vpar);
-            size.set(vpar, size.get(upar) + size.get(vpar));
-        }else{
-            parent.set(vpar, upar);
-            size.set(upar, size.get(upar) + size.get(vpar));
+        if(size[upar] <= size[vpar]){
+            size[vpar] += size[upar];
+            par[upar] = vpar;
+        }
+        else {
+            size[upar] += size[vpar];
+            par[vpar] = upar;
         }
     }
 
+    int find(int x){
+        if(x == par[x]) return x;
+        par[x] = find(par[x]);
+        return par[x];
+    }
     public List<List<String>> accountsMerge(List<List<String>> accounts) {
-
         int n = accounts.size();
-        initialize(n);
+        initialise(n);
 
         Map<String, Integer> mpp = new HashMap<>();
-
         for(int i = 0; i < n; i++){
             for(int j = 1; j < accounts.get(i).size(); j++){
                 String mail = accounts.get(i).get(j);
 
-                if(mpp.containsKey(mail)){
-                    UnionBySize(i, mpp.get(mail));
-                }else{
-                    mpp.put(mail, i);   // FIXED
-                }
+                if(mpp.containsKey(mail)) Union(i, mpp.get(mail));
+                else mpp.put(mail, i);
             }
         }
 
-        // 🔥 Create list to store emails of each parent
         List<List<String>> merged = new ArrayList<>();
         for(int i = 0; i < n; i++){
             merged.add(new ArrayList<>());
         }
 
         for(String mail : mpp.keySet()){
-            int parentIndex = findUPar(mpp.get(mail));
-            merged.get(parentIndex).add(mail);
+            int idx = find(mpp.get(mail));
+            merged.get(idx).add(mail);
         }
-
         List<List<String>> ans = new ArrayList<>();
-
         for(int i = 0; i < n; i++){
             if(merged.get(i).size() == 0) continue;
 
-            Collections.sort(merged.get(i));   // FIXED sorting
+            Collections.sort(merged.get(i));
 
             List<String> temp = new ArrayList<>();
-            temp.add(accounts.get(i).get(0));  // name
+            temp.add(accounts.get(i).get(0));
             temp.addAll(merged.get(i));
 
             ans.add(temp);

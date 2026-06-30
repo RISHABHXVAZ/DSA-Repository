72ca55eed4bc7f7c029cb41c1f466b50473d86bd
Rishabh Srivastava <srivastavarishabh799@gmail.com>
2026-06-30 14:18:55 +0530

    Time: 65 ms (95.01%), Space: 134.2 MB (87.12%) - LeetHub

--- 0827-making-a-large-island/0827-making-a-large-island.java
+++ 0827-making-a-large-island/0827-making-a-large-island.java
@@ -1,91 +1,86 @@
 class Solution {
-    List<Integer> parent = new ArrayList<>();
-    List<Integer> size = new ArrayList<>();
-    void initialize(int n){
+    int[] size;
+    int[] par;
+
+    void initialise(int n){
+        size = new int[n];
+        par = new int[n];
+
+        Arrays.fill(size, 1);
         for(int i = 0; i < n; i++){
-            parent.add(i);
-            size.add(1);
+            par[i] = i;
         }
     }
 
-    int findUPar(int u){
-        if(u == parent.get(u)) return u;
-        int par = findUPar(parent.get(u));
-        parent.set(u, par);
-        return par;
-    }
-    void UnionBySize(int u, int v){
-        int upar = findUPar(u);
-        int vpar = findUPar(v);
+    void Union(int u, int v){
+        int upar = find(u);
+        int vpar = find(v);
         if(upar == vpar) return;
-        else if(size.get(upar) < size.get(vpar)){
-            parent.set(upar, vpar);
-            size.set(vpar, size.get(vpar) + size.get(upar));
+
+        if(size[upar] <= size[vpar]){
+            size[vpar] += size[upar];
+            par[upar] = vpar;
         }else{
-            parent.set(vpar, upar);
-            size.set(upar, size.get(upar) + size.get(vpar));
+            size[upar] += size[vpar];
+            par[vpar] = upar;
         }
     }
 
-    boolean isValid(int adjr, int adjc, int n, int m){
-        return adjr >= 0 && adjr < n && adjc >= 0 && adjc < m;
+    int find(int x){
+        if(x == par[x]) return x;
+        par[x] = find(par[x]);
+        return par[x];
     }
     public int largestIsland(int[][] grid) {
         int n = grid.length;
-        int m = grid[0].length;
-        initialize(n*m);
-        int maxarea = Integer.MIN_VALUE;
-        boolean[][] vis = new boolean[n][m];
+        initialise(n*n);
+
+        int[] dx = {0,0,1,-1};
+        int[] dy = {1,-1,0,0};
+        boolean[] vis = new boolean[n*n];
         for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(grid[i][j] == 1 && !vis[i][j]){
-                    vis[i][j] = true;
-                    int nodeno = i*m + j;
-                    int dx[] = {1,-1,0,0};
-                    int dy[] = {0,0,1,-1};
-                    for(int k = 0; k < 4; k++){
-                        int adjr = i + dx[k];
-                        int adjc = j + dy[k];
-                        if(isValid(adjr, adjc, n , m)){
-                            int adjnode = adjr*m + adjc;
-                            if(vis[adjr][adjc]){
-                                if(findUPar(nodeno) != findUPar(adjnode)){
-                                    UnionBySize(nodeno, adjnode);
-                                }
-                            }
-                        }
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == 0) continue;
+                for(int k = 0; k < 4; k++){
+                    int nx = i + dx[k];
+                    int ny = j + dy[k];
+                    if(nx >= 0 && nx < n && ny >= 0 && ny < n && grid[nx][ny] == 1){
+                        Union(i*n+j, nx*n+ny);
                     }
                 }
             }
         }
 
-
+        int ans = Integer.MIN_VALUE;
         for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
+            for(int j = 0; j < n; j++){
                 if(grid[i][j] == 0){
-                    int dx[] = {1,-1,0,0};
-                    int dy[] = {0,0,1,-1};
-                    Set<Integer> components = new HashSet<>();
+                    int sum = 0;
                     for(int k = 0; k < 4; k++){
-                        int adjr = i + dx[k];
-                        int adjc = j + dy[k];
-                        if(isValid(adjr, adjc, n, m)){
-                            if(grid[adjr][adjc] == 1){
-                                components.add(findUPar(adjr*m + adjc));
-                            }
+                        int nx = i + dx[k];
+                        int ny = j + dy[k];
+                        if(nx >= 0 && nx < n && ny >= 0 && ny < n && grid[nx][ny] == 1){
+                            int upar = find(nx*n+ny);
+                            if(vis[upar]) continue;
+                            vis[upar] = true;
+                            sum += size[upar];
                         }
                     }
-                    int totalsize = 0;
-                    for(int it: components){
-                        totalsize += size.get(it);
+                    ans = Math.max(ans, sum+1);
+
+                    for(int k = 0; k < 4; k++){
+                        int nx = i + dx[k];
+                        int ny = j + dy[k];
+                        if(nx >= 0 && nx < n && ny >= 0 && ny < n){
+                            int upar = find(nx*n+ny);
+                            vis[upar] = false;
+                        }
                     }
-                    maxarea = Math.max(maxarea, totalsize+1);
                 }
-            }
+           }
         }
-        for(int cellno = 0; cellno < n*m; cellno++){
-            maxarea = Math.max(maxarea, size.get(findUPar(cellno)));
-        }
-        return maxarea;
+
+        return ans == Integer.MIN_VALUE ? n*n : ans;
+
     }
 }

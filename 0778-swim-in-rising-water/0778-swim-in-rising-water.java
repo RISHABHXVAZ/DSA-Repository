78844ad7a695ca7ccdb0e60e9341c3fc56a47db5
Rishabh Srivastava <srivastavarishabh799@gmail.com>
2026-06-30 14:33:55 +0530

    Time: 17 ms (16.8%), Space: 46.3 MB (75.81%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.java
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.java
@@ -1,44 +1,29 @@
 class Solution {
-    boolean isValid(int newr, int newc, int n){
-        return newr >= 0 && newr < n && newc >= 0 && newc < n;
-    }
     public int swimInWater(int[][] grid) {
         int n = grid.length;
-        int[][] dist = new int[n][n];
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[0]-b[0]);
+        pq.add(new int[]{grid[0][0], 0, 0});
         boolean[][] vis = new boolean[n][n];
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                dist[i][j] = Integer.MAX_VALUE;
-            }
-        }
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
-        pq.add(new int[] { grid[0][0], 0, 0 });
-        dist[0][0] = grid[0][0];
-        while (!pq.isEmpty()) {
-            int t = pq.peek()[0];
-            int r = pq.peek()[1];
-            int c = pq.peek()[2];
-            pq.remove();
-            int[] dx = { 1, -1, 0, 0 };
-            int[] dy = { 0, 0, 1, -1 };
-            for (int i = 0; i < 4; i++) {
-                int newr = r + dx[i];
-                int newc = c + dy[i];
-                if (isValid(newr, newc, n)) {
-                    if (!vis[newr][newc] && dist[newr][newc] > grid[newr][newc]) {
-                        vis[newr][newc] = true;
-                        if (grid[newr][newc] < t) {
-                            pq.add(new int[] { t, newr, newc });
-                            dist[newr][newc] = t;
-                        } else {
-                            pq.add(new int[] { grid[newr][newc], newr, newc });
-                            dist[newr][newc] = grid[newr][newc];
-                        }
-                    }
+        int[] dx = {1,-1,0,0};
+        int[] dy = {0,0,1,-1};
+        while(!pq.isEmpty()){
+            int[] p = pq.poll();
+            int t = p[0], r = p[1], c = p[2];
+            vis[r][c] = true;
+            if(r == n-1 && c == n-1) return t;
+
+            for(int i = 0; i < 4; i++){
+                int nx = r + dx[i];
+                int ny = c + dy[i];
+                if(nx >= 0 && nx < n && ny >= 0 && ny < n && !vis[nx][ny]){
+                    if(grid[nx][ny] > t) pq.add(new int[]{grid[nx][ny], nx, ny});
+                    else pq.add(new int[]{t, nx, ny});
                 }
             }
+
         }
-        return dist[n-1][n-1];
+
+        return -1;
     }
 }

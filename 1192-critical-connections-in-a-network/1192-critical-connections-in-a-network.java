789abed6dc850214c23d6531ef8689b2fdccd595
Rishabh Srivastava <srivastavarishabh799@gmail.com>
2026-07-02 12:25:54 +0530

    Time: 115 ms (18.24%), Space: 245.6 MB (35.18%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.java
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.java
@@ -1,39 +1,49 @@
 class Solution {
-    int timer = 1;
-    void dfs(int node, int parent, boolean[] vis, List<List<Integer>> adj, int[] tin, int[] low, List<List<Integer>> bridges){
+    int timer = 0;
+    void dfs(int node, int parent, int[] low, int[] tin, boolean[] vis, List<List<Integer>> adj, List<List<Integer>> ans){
         vis[node] = true;
         low[node] = tin[node] = timer;
         timer++;
+
         for(int it: adj.get(node)){
             if(it == parent) continue;
             if(!vis[it]){
-                dfs(it, node, vis, adj, tin, low, bridges);
-                low[node] = Math.min(low[it], low[node]);
+                dfs(it, node, low, tin, vis, adj, ans);
+                low[node] = Math.min(low[node], low[it]);
+
+                //node--------it      can we remove it? 
                 if(low[it] > tin[node]){
-                    bridges.add(Arrays.asList(it, node));
+                    ans.add(Arrays.asList(node, it));
                 }
-            }else{
-                low[node] = Math.min(low[it], low[node]);
-            }
+                }
+                else{
+                    low[node] = Math.min(low[node], low[it]);
+                }
         }
     }
     public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
         List<List<Integer>> adj = new ArrayList<>();
+
         for(int i = 0; i < n; i++){
             adj.add(new ArrayList<>());
         }
 
-        for(List<Integer> edges: connections){
-            int u = edges.get(0);
-            int v = edges.get(1);
+        for(int i = 0; i < connections.size(); i++){
+            List<Integer> temp = connections.get(i);
+            int u = temp.get(0);
+            int v = temp.get(1);
+
             adj.get(u).add(v);
             adj.get(v).add(u);
         }
-        boolean[] vis = new boolean[n];
+
+        List<List<Integer>> ans = new ArrayList<>();
         int[] tin = new int[n];
         int[] low = new int[n];
-        List<List<Integer>> bridges = new ArrayList<>();
-        dfs(0,-1,vis,adj,tin,low,bridges);
-        return bridges;
+        boolean[] vis = new boolean[n];
+
+        dfs(0, -1, low, tin, vis, adj, ans);
+
+        return ans;
     }
 }

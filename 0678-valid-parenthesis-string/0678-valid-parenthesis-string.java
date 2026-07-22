b09baa475da54ae6fc1d2bfc9799ffba16ac5ff9
Rishabh Srivastava <srivastavarishabh799@gmail.com>
2026-07-22 21:55:58 +0530

    Time: 1 ms (27.93%), Space: 44.4 MB (5.65%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -1,25 +1,27 @@
 class Solution {
-    public boolean checkValidString(String s) {
-     int n = s.length();
-     int min = 0, max = 0;
-     for(int i = 0; i < n; i++){
-        char ch = s.charAt(i);
-        if(ch == '('){
-            min++;
-            max++;
+    boolean func(int i, int count, String s, Boolean[][] dp){
+        if(count < 0) return false;
+        if(i == s.length()){
+            if(count == 0) return true;
+            return false;
         }
-        else if(ch == ')'){
-            min--;
-            max--;
-        }
+
+        if(dp[i][count] != null) return dp[i][count];
+        if(s.charAt(i) == '(') dp[i][count] = func(i+1, count+1, s, dp);
+        else if(s.charAt(i) == ')') dp[i][count] = func(i+1, count-1, s, dp);
         else{
-            min--;
-            max++;
+            boolean op1 = func(i+1, count+1, s, dp);
+            boolean op2 = func(i+1, count-1, s, dp);
+            boolean op3 = func(i+1, count, s, dp);
+
+            dp[i][count] = op1 || op2 || op3;
         }
-        if(min < 0) min = 0;
-        if(max < 0) return false;
+
+        return dp[i][count];
     }
-    if(min == 0) return true;
-    else return false;   
+    public boolean checkValidString(String s) {
+        int n = s.length();
+        Boolean[][] dp = new Boolean[n][n+5];
+        return func(0,0,s,dp);
     }
 }

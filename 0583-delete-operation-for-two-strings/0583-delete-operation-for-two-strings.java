d82b6c395b97250a24bbbc147aaa093646d7facc
Rishabh Srivastava <srivastavarishabh799@gmail.com>
2026-07-22 11:10:30 +0530

    Time: 8 ms (20.9%), Space: 46.6 MB (14.11%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
@@ -1,36 +1,23 @@
 class Solution {
     int func(int i, int j, String word1, String word2, int[][] dp){
         if(i == word1.length() && j == word2.length()) return 0;
-
         if(i == word1.length()) return word2.length() - j;
-        if(j == word2.length()) return word1.length() - i;
-
+        if(j == word2.length()) return word1.length() - i;  
         if(dp[i][j] != -1) return dp[i][j];
 
         if(word1.charAt(i) == word2.charAt(j)) dp[i][j] = func(i+1,j+1,word1,word2,dp);
-        else dp[i][j] = 1 + Math.min(func(i,j+1,word1,word2,dp), func(i+1,j,word1,word2,dp));
+        else{
+            dp[i][j] = 1 + Math.min(func(i+1,j,word1, word2,dp), func(i, j+1, word1, word2,dp));
+        }
 
         return dp[i][j];
     }
-    public int minDistance(String word1, String word2){
-        int l1 = word1.length();
-        int l2 = word2.length();
+    public int minDistance(String word1, String word2) {
+        int n1 = word1.length();
+        int n2 = word2.length();
 
-       int[] curr = new int[l2+1];
-        int[] next = new int[l2+1];
-
-        for(int j = 0; j <= l2; j++) next[j] = l2 - j;
-        
-        for(int i = l1-1; i >= 0; i--){
-            curr[l2] = l1 - i;
-            for(int j = l2-1; j >= 0; j--){
-                if(word1.charAt(i) == word2.charAt(j)) curr[j] = next[j+1];
-                else curr[j] = 1 + Math.min(curr[j+1], next[j]);
-            }
-            next = curr;
-            curr = new int[l2+1];
-        }
-
-        return next[0];
+        int[][] dp = new int[n1][n2];
+        for(int i = 0; i < n1; i++) Arrays.fill(dp[i], -1);
+        return func(0,0,word1,word2,dp);
     }
 }

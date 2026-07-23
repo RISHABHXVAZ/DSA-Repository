af4ee40c9d1af57291ecaaa52ebde76d74c173a6
Rishabh Srivastava <srivastavarishabh799@gmail.com>
2026-07-23 20:14:53 +0530

    Time: 11 ms (14.98%), Space: 49.2 MB (42.87%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -3,28 +3,26 @@ class Solution {
         int n = intervals.length;
         Arrays.sort(intervals, (a,b)->a[0]-b[0]);
 
-        List<int[]> lst = new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
         for(int i = 0; i < n; i++){
-            if(lst.isEmpty() || intervals[i][0] > lst.get(lst.size()-1)[1]){
-                lst.add(intervals[i]);
+            if(ans.isEmpty() || intervals[i][0] > ans.get(ans.size()-1).get(1)){
+                ans.add(Arrays.asList(intervals[i][0], intervals[i][1]));
                 continue;
             }
 
-            int[] temp = lst.get(lst.size()-1);
-            if(intervals[i][0] <= temp[1]){
-                lst.remove(lst.size()-1);
-                temp[0] = Math.min(temp[0], intervals[i][0]);
-                temp[1] = Math.max(temp[1], intervals[i][1]);
-                lst.add(temp);
-            }
+            List<Integer> last = ans.get(ans.size()-1);
+            last.set(0, Math.min(last.get(0), intervals[i][0]));
+            last.set(1, Math.max(last.get(1), intervals[i][1]));
         }
 
-        int[][] ans = new int[lst.size()][2];
+        int[][] res = new int[ans.size()][2];
         int k = 0;
-        for(int[] p : lst){
-            ans[k++] = p;
+        for(List<Integer> temp : ans){
+            res[k][0] = temp.get(0);
+            res[k][1] = temp.get(1);
+            k++; 
         }
-        
-        return ans;
+
+        return res;
     }
 }

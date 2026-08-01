60fc0ff87d0761b252cbae8f94544827be6f54c6
Rishabh Srivastava <srivastavarishabh799@gmail.com>
2026-08-01 23:13:19 +0530

    Time: 24 ms (22.91%), Space: 48.5 MB (5.04%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,29 +1,41 @@
 class Solution {
     public String minWindow(String s, String t) {
-        int m = s.length();
-        int n = t.length();
-        int[] hash = new int[256];
-        for(int i = 0; i < n; i++){
-           hash[t.charAt(i)]++;
+        int n = s.length();
+        int m = t.length();
+
+        Map<Character, Integer> target = new HashMap<>();
+        Map<Character, Integer> mpp = new HashMap<>();
+        for(int i = 0; i < m; i++){
+            target.put(t.charAt(i), target.getOrDefault(t.charAt(i), 0) + 1);
         }
-        int i = 0, j = 0, minlen = Integer.MAX_VALUE;
-        int cnt = 0;
-        int startIndex = -1, endIndex = -1;
-        while(j < m){
-            if(hash[s.charAt(j)] > 0) cnt++;
-            hash[s.charAt(j)]--;
-            while(cnt == n){
-                if(j-i+1 < minlen){
-                    minlen = j-i+1;
-                    startIndex = i;
-                    endIndex = j;
+
+        int i = 0, j = 0;
+        int formed = 0, ans = Integer.MAX_VALUE;
+        int startI = -1, endI = -1;
+        while(i < n && j < n){
+            char ch = s.charAt(j);
+            mpp.put(ch, mpp.getOrDefault(ch,0)+1);
+
+            if(target.containsKey(ch) && mpp.containsKey(ch) && target.get(ch).intValue() == mpp.get(ch).intValue()){
+                formed++;
+            }
+
+            while(formed == target.size()){
+                if(j-i+1 < ans){
+                    ans = j-i+1;
+                    startI = i;
+                    endI = j;
                 }
-                hash[s.charAt(i)]++;
-                if(hash[s.charAt(i)] > 0) cnt--;
+                if(target.containsKey(s.charAt(i)) && mpp.get(s.charAt(i)).intValue() == target.get(s.charAt(i)).intValue()){
+                    formed--;
+                }
+                mpp.put(s.charAt(i), mpp.get(s.charAt(i))-1);
+                if(mpp.get(s.charAt(i)) == 0) mpp.remove(s.charAt(i));
                 i++;
             }
             j++;
         }
-        return startIndex == -1 ? "" : s.substring(startIndex,endIndex+1);
+        if(startI == -1 && endI == -1) return "";
+        return s.substring(startI, endI+1);
     }
 }

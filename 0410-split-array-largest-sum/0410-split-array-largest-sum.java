38a39333f0c36aa88a5e3cf9f114919f12d470d2
Rishabh Srivastava <srivastavarishabh799@gmail.com>
2026-08-06 23:37:41 +0530

    Time: 0 ms (100%), Space: 43.1 MB (33.18%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.java
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -1,35 +1,39 @@
 class Solution {
-    int divisions(int[] nums, int maxsum){
+    int partitions(int[] arr, int maxsum){
+        int n = arr.length;
+        int p = 0;
         int sum = 0;
-        int div = 1;
-
-        for(int i = 0; i < nums.length; i++){
-            if(sum + nums[i] <= maxsum){
-                sum += nums[i];
-            }else{
-                sum = nums[i];
-                div++;
+        for(int i = 0; i < n; i++){
+            if(arr[i] > maxsum) return Integer.MAX_VALUE;
+            if(sum + arr[i] > maxsum){
+                p++;
+                sum = arr[i];
+                continue;
             }
+            
+            sum += arr[i];
         }
 
-        return div;
+        return p+1;
     }
     public int splitArray(int[] nums, int k) {
         int n = nums.length;
-        int max = -1, sum = 0;
+        int sum = 0;
         for(int i = 0; i < n; i++){
-            max = Math.max(max, nums[i]);
             sum += nums[i];
         }
 
-        int low = max, high = sum;
+        int low = 0, high = sum;
+        int ans = 0;
         while(low <= high){
             int mid = low + (high-low)/2;
-            if(divisions(nums, mid) <= k){
+
+            if(partitions(nums, mid) <= k){
                 high = mid-1;
+                ans = mid;
             }else low = mid+1;
         }
 
-        return low;
+        return ans;
     }
 }

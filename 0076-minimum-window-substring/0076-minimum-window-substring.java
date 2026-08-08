c5f245581095a91189a6ae68b67fb6c02cc931b0
Rishabh Srivastava <srivastavarishabh799@gmail.com>
2026-08-08 15:19:04 +0530

    Time: 26 ms (21.35%), Space: 48 MB (13.96%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -3,39 +3,43 @@ class Solution {
         int n = s.length();
         int m = t.length();
 
-        Map<Character, Integer> target = new HashMap<>();
         Map<Character, Integer> mpp = new HashMap<>();
+        Map<Character, Integer> target = new HashMap<>();
+
         for(int i = 0; i < m; i++){
             target.put(t.charAt(i), target.getOrDefault(t.charAt(i), 0) + 1);
         }
 
-        int i = 0, j = 0;
-        int formed = 0, ans = Integer.MAX_VALUE;
+        int formed = 0;
+        int ans = Integer.MAX_VALUE;
         int startI = -1, endI = -1;
-        while(i < n && j < n){
+        int i = 0;
+        for(int j = 0; j < n; j++){
             char ch = s.charAt(j);
             mpp.put(ch, mpp.getOrDefault(ch,0)+1);
 
-            if(target.containsKey(ch) && mpp.containsKey(ch) && target.get(ch).intValue() == mpp.get(ch).intValue()){
+            if(target.containsKey(ch) && target.get(ch).intValue() == mpp.get(ch).intValue()){
                 formed++;
             }
-
-            while(formed == target.size()){
-                if(j-i+1 < ans){
+                while(i <= j && formed == target.size()){
+                    if(j-i+1 < ans){
                     ans = j-i+1;
                     startI = i;
                     endI = j;
+                    }
+                    char ch1 = s.charAt(i);
+                    mpp.put(ch1, mpp.get(ch1)-1);
+                    if(mpp.get(ch1) == 0) mpp.remove(ch1);
+
+                    if(target.containsKey(ch1) && (!mpp.containsKey(ch1) || mpp.get(ch1).intValue() < target.get(ch1).intValue())){
+                        formed--;
+                    }
+                    i++;
                 }
-                if(target.containsKey(s.charAt(i)) && mpp.get(s.charAt(i)).intValue() == target.get(s.charAt(i)).intValue()){
-                    formed--;
-                }
-                mpp.put(s.charAt(i), mpp.get(s.charAt(i))-1);
-                if(mpp.get(s.charAt(i)) == 0) mpp.remove(s.charAt(i));
-                i++;
-            }
-            j++;
         }
+
         if(startI == -1 && endI == -1) return "";
         return s.substring(startI, endI+1);
+
     }
 }

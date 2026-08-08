1f45a6f511bb958e0a1bfe091591e40443184349
Rishabh Srivastava <srivastavarishabh799@gmail.com>
2026-08-08 14:46:36 +0530

    Time: 51 ms (28.24%), Space: 50.4 MB (14.99%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
@@ -1,21 +1,22 @@
 class Solution {
-    static int func(int[] nums, int k){
-        if(k < 0) return 0;
+    int func(int[] nums, int k){
         int n = nums.length;
-        int i = 0, j = 0;
-        int cnt = 0;
+        int count = 0;
         Map<Integer, Integer> mpp = new HashMap<>();
-        while(j < n){
-            mpp.put(nums[j], mpp.getOrDefault(nums[j],0) + 1);
-            while(mpp.size() > k){
-                mpp.put(nums[i], mpp.get(nums[i]) - 1);
+
+        int i = 0;
+        for(int j = 0; j < n; j++){
+            mpp.put(nums[j], mpp.getOrDefault(nums[j], 0)+1);
+
+            while(i <= j && mpp.size() > k){
+                mpp.put(nums[i], mpp.get(nums[i])-1);
                 if(mpp.get(nums[i]) == 0) mpp.remove(nums[i]);
                 i++;
             }
-            cnt += (j-i+1);
-            j++;
+
+           count += (j-i+1);
         }
-        return cnt;
+        return count;
     }
     public int subarraysWithKDistinct(int[] nums, int k) {
         return func(nums, k) - func(nums, k-1);

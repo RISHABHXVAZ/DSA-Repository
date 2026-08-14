b3615b503460bb9c66c16e0b0da7d88d976fcf05
Rishabh Srivastava <srivastavarishabh799@gmail.com>
2026-08-14 23:07:01 +0530

    Time: 0 ms (100%), Space: 43 MB (47.55%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -1,41 +1,14 @@
 class Solution {
-    void reverse(int[] temp, int i, int j){
-        while(i < j){
-            int t = temp[i];
-            temp[i] = temp[j];
-            temp[j] = t;
-            i++;
-            j--;
-        }
-
-        return;
-    }
-    void rotate(int[] temp, int d){
-        reverse(temp, 0, d-1);
-        reverse(temp, d, temp.length-1);
-        reverse(temp, 0, temp.length-1);
-    }
     public boolean check(int[] nums) {
         int n = nums.length;
-        int[] temp = new int[n];
-        for(int i = 0; i < n; i++){
-            temp[i] = nums[i];
-        }
-
-        Arrays.sort(temp);
+        int cnt = 0;
 
         for(int i = 0; i < n; i++){
-            rotate(temp, 1);
-            int flag = 0;
-            for(int j = 0; j < n; j++){
-                if(temp[j] != nums[j]){
-                    flag = 1;
-                    break;
-                }
+            if(nums[i] > nums[(i+1)%n]){
+                cnt++;
             }
-            if(flag == 0) return true;
         }
 
-        return false;
+        return cnt <= 1;
     }
 }

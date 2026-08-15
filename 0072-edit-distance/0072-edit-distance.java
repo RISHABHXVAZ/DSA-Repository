fe306993f0aa2ff2aebf9e0087dc94d838d0952e
Rishabh Srivastava <srivastavarishabh799@gmail.com>
2026-08-15 19:31:43 +0530

    Time: 5 ms (66.11%), Space: 47.1 MB (53.29%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -17,10 +17,32 @@ class Solution {
         return dp[i][j];
     }
     public int minDistance(String word1, String word2) {
-        int[][] dp = new int[word1.length()][word2.length()];
-        for(int i = 0; i < word1.length(); i++){
-            Arrays.fill(dp[i], -1);
+        int n1 = word1.length();
+        int n2 = word2.length();
+
+        int[][] dp = new int[n1+1][n2+1];
+       
+       for(int i = 0; i < n1; i++){
+        dp[i][n2] = word1.length()-i;
+       }
+
+       for(int i = 0; i < n2; i++){
+        dp[n1][i] = word2.length()-i;
+       }
+
+       for(int i = n1-1; i >= 0; i--){
+        for(int j = n2-1; j >= 0; j--){
+            if(word1.charAt(i) == word2.charAt(j)){
+                dp[i][j] = dp[i+1][j+1];
+            }else{
+                int op1 = 1 + dp[i][j+1];
+                int op2 = 1 + dp[i+1][j];
+                int op3 = 1 + dp[i+1][j+1];
+
+                dp[i][j] = Math.min(op1, Math.min(op2, op3));
+            }
         }
-        return func(0, 0, word1, word2, dp);
+       }
+     return dp[0][0];
     }
 }

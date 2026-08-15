d5f3fedcb95391615cafb21ecb2b3a00b5dec30d
Rishabh Srivastava <srivastavarishabh799@gmail.com>
2026-08-15 20:29:46 +0530

    Time: 160 ms (5.01%), Space: 46.8 MB (64.7%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -8,54 +8,44 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class pair{
-    ListNode node;
-    int idx;
-
-    pair(ListNode node, int i){
-        this.node = node;
-        this.idx = i;
-    }
-}
 class Solution {
-    public ListNode mergeKLists(ListNode[] lists) {
-        int n = lists.length;
-        ListNode[] help = new ListNode[n];
-
-        PriorityQueue<pair> pq = new PriorityQueue<>((a,b) -> {
-            if(a.node.val != b.node.val) return Integer.compare(a.node.val, b.node.val);
-            else return Integer.compare(a.idx, b.idx);
-        });
-
-        for(int i = 0; i < n; i++){
-            ListNode node = lists[i];
-            if(node != null){
-                pq.add(new pair(node, i));
-            help[i] = node.next;
+    ListNode merge(ListNode h1, ListNode h2){
+        ListNode t1 = h1, t2 = h2, t3 = new ListNode(-1);
+        ListNode head = t3;
+        while(t1 != null && t2 != null){
+            if(t1.val < t2.val){
+                t3.next = t1;
+                t1 = t1.next;
+            }else{
+                t3.next = t2;
+                t2 = t2.next;
             }
+            t3 = t3.next;
         }
 
+        while(t1 != null){
+            t3.next = t1;
+            t1 = t1.next;
+            t3 = t3.next;
+        }
+
+        while(t2 != null){
+            t3.next = t2;
+            t2 = t2.next;
+            t3 = t3.next;
+        }
+
+        return head.next;
+    }
+    public ListNode mergeKLists(ListNode[] lists) {
+        int n = lists.length;
+
         ListNode head = null;
-        ListNode temp = head;
-        while(!pq.isEmpty()){
-            pair p = pq.poll();
+        for(int i = 0; i < n; i++){
+            ListNode head1 = lists[i];
+            head = merge(head, head1);
+        }
 
-            if(head == null){ 
-                head = p.node;
-                temp = p.node;
-
-                if(help[p.idx] != null) pq.add(new pair(help[p.idx], p.idx));
-                if(help[p.idx] != null) help[p.idx] = help[p.idx].next;
-                continue;
-            }
-
-            temp.next = p.node;
-            temp = temp.next;
-            if(help[p.idx] != null) pq.add(new pair(help[p.idx], p.idx));
-            if(help[p.idx] != null) help[p.idx] = help[p.idx].next;
-        } 
-
-
-        return head; 
+        return head;
     }
 }

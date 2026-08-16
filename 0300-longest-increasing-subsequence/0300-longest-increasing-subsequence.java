526fa7810b853e797c21b0142f21c2f829a0728e
Rishabh Srivastava <srivastavarishabh799@gmail.com>
2026-08-16 22:49:31 +0530

    Time: 372 ms (13.72%), Space: 237.3 MB (11.23%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,27 +1,30 @@
 class Solution {
+    int func(int i, int prev, int[] nums, int[][] dp){
+        if(i == nums.length) return 0;
+
+        if(dp[i][prev+1] != -1) return dp[i][prev+1];
+        int take = Integer.MIN_VALUE;
+        if(prev == -1 || nums[i] > nums[prev]){
+            take = 1 + func(i+1, i, nums, dp);
+        }
+        int nottake = func(i+1, prev, nums, dp);
+        return dp[i][prev+1] = (int)Math.max(take, nottake);
+    }
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
 
-        List<Integer> temp = new ArrayList<>();
-        int size = 0;
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i < n; i++) max = Math.max(max, nums[i]);
 
-        for(int x: nums){
-            int left = 0;
-            int right = size;
+        int maxlen = 0;
+        int[][] dp = new int[n][n];
+        for(int i = 0; i < n; i++) Arrays.fill(dp[i], -1);
 
-            while(left < right){
-                int mid = left + (right-left)/2;
-
-                if(temp.get(mid) < x){
-                    left = mid+1;
-                }else right = mid;
-            }
-
-            if(left == size){
-                temp.add(x);
-                size++;
-            }else temp.set(left, x);
+        for(int i = 0; i < n; i++){ 
+            int len = func(i, -1, nums, dp);
+            maxlen = Math.max(maxlen, len);
         }
-        return size;
+
+        return maxlen;
     }
 }

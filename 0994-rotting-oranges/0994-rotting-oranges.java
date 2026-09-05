0edbbbc09de1f538cb7ab43f2ab2660ba18cb5cc
Rishabh Srivastava <srivastavarishabh799@gmail.com>
2026-09-05 14:59:25 +0530

    Time: 2 ms (88.83%), Space: 43.9 MB (84.38%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,46 +1,47 @@
 class Solution {
-    int bfs(Queue<int[]> q, int[][] grid){
-        int time = 0;
-        int[] dx = {-1,1,0,0};
-        int[] dy = {0,0,-1,1};
-        while(!q.isEmpty()){
-            int size = q.size();
-            time++;
-            for(int i = 0; i < size; i++){
-                int[] p = q.poll();
-                for(int j = 0; j < 4; j++){
-                    int nx = p[0] + dx[j];
-                    int ny = p[1] + dy[j];
-                    if(nx >= 0 && nx < grid.length && ny >= 0 && ny < grid[0].length && grid[nx][ny] == 1){
-                        grid[nx][ny] = 2;
-                        q.add(new int[]{nx, ny});
-                    }
-                }
-            }
-        }
-
-        return time > 0? time-1: 0;
-    }
     public int orangesRotting(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        Queue<int[]> q = new LinkedList<>();
+        int n = grid.length;
+        int m = grid[0].length;
 
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
+        Queue<int[]> q = new LinkedList<>();
+        boolean[][] vis = new boolean[n][m];
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
                 if(grid[i][j] == 2){
+                    vis[i][j] = true;
                     q.add(new int[]{i,j});
                 }
             }
         }
 
-        int time = bfs(q, grid);
+        int[] dx = {1, -1, 0, 0};
+        int[] dy = {0, 0, 1, -1};
 
-        for(int i = 0; i < m; i++){
-            for(int num : grid[i]){
-                if(num == 1) return -1;
+        int time = -1;
+        while(!q.isEmpty()){
+            int size = q.size();
+            time++;
+            for(int i = 0; i < size; i++){
+                int[] p = q.poll();
+                int r = p[0], c = p[1];
+                
+                for(int j = 0; j < 4; j++){
+                    int nr = r + dx[j];
+                    int nc = c + dy[j];
+                    if(nr >= 0 && nr < n && nc >= 0 && nc < m && !vis[nr][nc] && grid[nr][nc] == 1){
+                        vis[nr][nc] = true;
+                        q.add(new int[]{nr, nc});
+                    }
+                }
             }
         }
-        return time;
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(grid[i][j] == 1 && !vis[i][j]) return -1;
+            }
+        }
+
+        return time == -1 ? 0 : time;
     }
 }
